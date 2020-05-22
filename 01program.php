043ee229e71048da5746ca3bdd89ab69e3a12029
Azaradael <65790849+Azaradael@users.noreply.github.com>
2020-05-22 23:51:11 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -1,5 +1,8 @@
 <?php 
 echo ("Witaj");
 echo("<br> Jestem programem 01");
-echo ("<br>Mam nadzieje ze mi wyszlo!")
+echo ("zadanie samodzielne)Mam nadzieje ze mi wyszlo!");
+echo ("Teraz edytuje w Notepad++!");
+echo("Dalsza czesc programu");
+echo("Dalsza czesc programu, ale tym razem edytuje go przez GitHuba!");
 ?>
